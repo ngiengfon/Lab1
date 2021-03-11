127bf16a96b61d98e1ff6de0301ee3a140de1ad0
SFNgieng <senfon88@gmail.com>
2021-03-11 14:20:23 +0800

    New to Python 2

---
+++ Test.py
@@ -0,0 +1,12 @@
+A = [41,51,69,36,51,68]
+def InsertionSort(list1):
+    for j in range (1,len(A),1):
+        key= A[j]
+        i = j - 1
+        while (i>=0 and A[i]<key):
+            A[i+1]=A[i]
+            i =i-1
+            A[i+1]=key
+
+InsertionSort(A)
+print(A)
